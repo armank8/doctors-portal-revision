67fdb072afe02422f952198bc59252a6dce563df
Arman-Kabir <armankabir8@gmail.com>
2024-05-22 09:30:07 +0600

    banner and navbar

---
+++ client/src/pages/Home/Banner.jsx
@@ -0,0 +1,21 @@
+import chair from "../../assets/images/chair.png";
+export default function Banner() {
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-100">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,21 @@ export default {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
   plugins: [
     daisyui,
   ],
