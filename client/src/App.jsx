5dc48c7151cac9e53a0fd50137fb173c52c04b90
Arman-Kabir <armankabir8@gmail.com>
2024-05-22 09:12:20 +0600

    few initial pages created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./pages/shared/Navbar";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 export default function App() {
   return (
-    <div>
-      
+    <div className="">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="about" element={<About></About>}></Route>
+      </Routes>
     </div>
-  )
+  );
 }
