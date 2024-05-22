65c0d1095a1af2fa774b43d1e43cbac507caa89b
Arman-Kabir <armankabir8@gmail.com>
2024-05-22 12:59:30 +0600

    service done

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Banner from "./Banner";
 import Info from "./Info";
+import Services from "./Services";
 
 
 export default function Home() {
   return (
-    <div>
+    <div className="px-12">
         <Banner></Banner>
         <Info></Info>
+        <Services></Services>
     </div>
   )
 }
---
+++ client/src/pages/Home/Services.jsx
@@ -0,0 +1,45 @@
+import Service from "./Service";
+import fluoride from "../../assets/images/fluoride.png";
+import cavity from "../../assets/images/cavity.png";
+import whitening from "../../assets/images/whitening.png";
+
+export default function Services() {
+  const services = [
+    {
+      _id: 1,
+      name: "Fluoride Treatment",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur quo, iste itaque omnis nemo deleniti veritatis impedit eius quae sed?",
+      img: fluoride,
+    },
+    {
+      _id: 2,
+      name: "Cavity Filling",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur quo, iste itaque omnis nemo deleniti veritatis impedit eius quae sed?",
+      img: cavity,
+    },
+    {
+      _id: 3,
+      name: "Teeth Whitening",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur quo, iste itaque omnis nemo deleniti veritatis impedit eius quae sed?",
+      img: whitening,
+    },
+  ];
+  return (
+    <div className="my-28">
+      <div className="text-center">
+        <h3 className="text-primary  text-xl font-bold uppercase">
+          Our Services
+        </h3>
+        <h2 className="text-4xl">Services we Provide</h2>
+      </div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+        {services.map((service) => (
+          <Service key={service._id} service={service}></Service>
+        ))}
+      </div>
+    </div>
+  );
+}
